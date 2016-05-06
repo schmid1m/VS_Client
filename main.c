354c85f5b17b2a528f03b675ba27ff477a1032e2
Michel Schmidt <schmidtbytes@gmx.de>
2016-05-06 19:43:35 +0200

    Minor code improvements for better visibility
    Added Block length for extended Tests
    fixed double pointer errror with out data
    Unroled inline for loops to increase Visibility

--- main.c
+++ main.c
@@ -6,21 +6,28 @@
 **  Project     : Verteilte Systeme Labor                    **
 **************************************************************/
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-#include "api/Macros.h"
-#include "api/clientAPI.h"
-#include "api/commonAPI.h"
+#include "VS_LAB/Macros.h"
+#include "VS_LAB/clientAPI.h"
+#include "VS_LAB/commonAPI.h"
 
+#define BLOCKLEN 8
+
+// Arguments:
+// vslabc <infile> <outfile> <ip>
 int main(int argc, char **argv)
 {
-	uint8_t gp[2], out_data[4], iReturn, *ip_ptr;
-	uint16_t in_data[4], blockID = 0;
+    uint8_t gp[2], **out_data, iReturn, *ip_ptr;
+    uint16_t in_data[BLOCKLEN], blockID = 0;
 	uint32_t senderIP, length;
 	FILE *fpIn, *fpOut;
 	msg msg_ptr;
 	FID msg_type;
+
+    out_data = malloc(sizeof(uint8_t*));
 
 	// introduce yourself
 	printf("VSLab client, build %s %s\n", __DATE__, __TIME__);
@@ -65,17 +72,25 @@ int main(int argc, char **argv)
 		return -1;
 	} else {
 		ip_ptr = (uint8_t*)&senderIP;
-		printf("Client: Received gp response from %d.%d.%d.%d\n", ip_ptr[3],ip_ptr[2],ip_ptr[1],ip_ptr[0]);
+        printf("Client: Received gp response from %d.%d.%d.%d\n", ip_ptr[3],ip_ptr[2],ip_ptr[1],ip_ptr[0]);
 		free_msg(&msg_ptr);
 	}
 
 	/*****************************************************************************/
 
 	// read in data & send it out
-	fread(&in_data,sizeof(uint16_t),sizeof(in_data)/sizeof(uint16_t),fpIn);
-	for(int i=0; i<sizeof(in_data)/sizeof(uint16_t); i++) in_data[i] = htons(in_data[i]); // correcting the byteorder
-	printf("Client: Read in data: "); for(int i=0; i<sizeof(in_data)/sizeof(uint16_t); i++) printf("%#x ", in_data[i]); printf("\n");
-	iReturn = send_dec_req(blockID, in_data, sizeof(in_data)/sizeof(uint16_t), inet_network(argv[3]));
+    fread(&in_data,sizeof(uint16_t),BLOCKLEN,fpIn);
+    for(uint32_t i=0; i<BLOCKLEN; i++)
+    {
+        in_data[i] = htons(in_data[i]); // correcting the byteorder
+    }
+    printf("Client: Read in data: ");
+    for(uint32_t i=0; i<BLOCKLEN; i++)
+    {
+        printf("%#x ", in_data[i]);
+    }
+    printf("\n");
+    iReturn = send_dec_req(blockID, in_data, BLOCKLEN, inet_network(argv[3]));
 	if(iReturn != NO_ERROR) {
 		printf("Client: Sending out data was not successful: %d\n", iReturn);
 		return -1;
@@ -92,30 +107,49 @@ int main(int argc, char **argv)
 	} else {
 		ip_ptr = (uint8_t*)&senderIP;
 		printf("Client: Received data response from %d.%d.%d.%d\n", ip_ptr[3],ip_ptr[2],ip_ptr[1],ip_ptr[0]);
-		free_msg(&msg_ptr);
 	}
 
 	// print result
-	iReturn = extract_dec_rsp(&msg_ptr, &blockID, (uint8_t**)&out_data, &length);
+    iReturn = extract_dec_rsp(&msg_ptr, &blockID, out_data, &length);
 	if(iReturn != NO_ERROR) {
 		printf("Client: An error occured during extracting the data packet: %d\n", iReturn);
 		return -1;
 	} else {
+        (*out_data)[length] = '\0';
 		printf("        with length %d and seq nr %d\n", length, blockID);
-		printf("Client: Received data: "); for(int i=0; i<sizeof(out_data); i++) printf("%#x ", out_data[i]); printf("\n");
+        printf("Client: Received data: %s \n", *out_data);
 		free_msg(&msg_ptr);
 	}
 
 	/*****************************************************************************/
 
-	// deinit socket
+    iReturn = send_unlock_req(inet_network(argv[3]));
+    if(iReturn != NO_ERROR) {
+        printf("Client: Sending unlock request was not successful: %d\n", iReturn);
+        return -1;
+    } else {
+        printf("Client: Sending unlock request worked\n");
+    }
+
+    // receive unlock response
+    iReturn = recv_msg(&msg_ptr, &senderIP);
+    msg_type = get_msg_type(&msg_ptr);
+    if(iReturn != NO_ERROR && msg_type == UNLOCK_RSP) {
+        printf("Client: No unlock response received: %d\n", iReturn);
+        return -1;
+    } else {
+        ip_ptr = (uint8_t*)&senderIP;
+        printf("Client: Received unlock response from %d.%d.%d.%d\n", ip_ptr[3],ip_ptr[2],ip_ptr[1],ip_ptr[0]);
+    }
+
+    // deinit socket
 	deinit_client();
 
 	// close filepointer
 	fclose(fpIn);
 	fclose(fpOut);
 
-    printf("Client: Scrambling %s is done!", argv[1]);
+    printf("Client: Scrambling %s is done!\n", argv[1]);
 
     return 0;
 }
--- vs_client.pro
+++ vs_client.pro
@@ -1,3 +1,4 @@
+TARGET = vslabc
 TEMPLATE = app
 CONFIG += console c++11
 CONFIG -= app_bundle
